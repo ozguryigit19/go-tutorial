738993a651d5ce540416921e460ca615b2802f05
ozguryigit <serhatyilmaz@Serhat-MacBook-Pro.local>
2021-03-13 01:18:27 +0200

    variables and declarations

---
+++ values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var var1 = "variable 1"
+	fmt.Println(var1)
+
+	const b, c int = 1, 2
+	fmt.Println(b, c)
+
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+}
